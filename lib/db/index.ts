79ea552f5bf892b540d3462f4e7733f740747398
Raynold Gyasi <speedyray2ray@gmail.com>
2025-05-17 21:27:34 -0400

    first commit

---
+++ lib/db/index.ts
@@ -0,0 +1,15 @@
+import {drizzle } from "drizzle-orm/neon-http"
+import {neon} from "@neondatabase/serverless"
+import * as schema from "./schema"
+
+
+// Validate environment variables
+if (!process.env.DATABASE_URL) {
+    throw new Error("Missing DATABASE_URL in environment");
+  }
+  
+const sql = neon(process.env.DATABASE_URL!);
+
+export const db = drizzle(sql, {schema});
+
+export {sql}
